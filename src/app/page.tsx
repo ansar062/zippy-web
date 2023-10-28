2eeb92bc378b0afc22dd0347d71afd8868b9f9a6
hamza khalid <hamzakhalid10002000@gmail.com>
2023-10-28 23:15:27 +0500

    SignUp page is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <Navbar/>
       {/* <Hero/> */}
       {/* <Burger /> */}
-     
-     <LoginPage/>
+     <SignUpPage/>
+   
       {/* <Aboutus /> */}
      
     </div>
