de97b4dc3a948a3c255b3a54c12a597aef702530
Muhammad Ansar <109860407+ansar062@users.noreply.github.com>
2023-10-30 15:17:36 +0500

    unresponsive cartpage with animation

---
+++ src/app/components/ShoppingCart/ShoppingCart.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function ShoppingCart() {
+    return (
+        <div className="flex min-h-screen bg-white">
+            <motion.div 
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 1 }}
+            
+            className="flex px-[20px] py-[2rem] space-y-[3rem] w-[80rem] text-black text-[40px] flex-col items-center bg-white">
+                <div className="flex font-semibold space-x-[40rem]">
+                    <h1>Shopping Cart</h1>
+                    <h1>3 Items</h1>
+                </div>
+                <hr className="h-[2px] bg-[#EB002B] w-full" />
+                <div className="flex space-x-[25rem] flex-shrink justify-evenly">
+                    <h1>Product Details</h1>
+                    <div className="flex space-x-[15rem]">
+                        <h1>Qty</h1>
+                        <h1>Price</h1>
+                    </div>
+                </div>
+                <div className="flex space-x-[15rem] flex-shrink justify-evenly">
+                    <div className="flex justify-center items-center">
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{
+                                duration: 0.8,
+                                delay: 0.5,
+                                ease: [0, 0.71, 0.2, 1.01]
+                            }}
+                        >
+                            <img src="images/image-1.png" alt="" />
+                        </motion.div>
+
+                        <h1 className="text-[36px]">Mighty Zinger</h1>
+                    </div>
+                    <div className="flex justify-center items-center space-x-[15rem]">
+                        <h1>1</h1>
+                        <h1>850</h1>
+                    </div>
+                </div>
+                <div className="flex space-x-[15rem] flex-shrink justify-evenly">
+                    <div className="flex justify-center items-center">
+                    <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{
+                                duration: 0.8,
+                                delay: 0.5,
+                                ease: [0, 0.71, 0.2, 1.01]
+                            }}
+                        >
+                            <img src="images/screenshot-202308021130211.png" alt="" />
+                        </motion.div>
+                        
+                        <h1 className="text-[36px]">Krunch Burger</h1>
+                    </div>
+                    <div className="flex justify-center items-center space-x-[15rem]">
+                        <h1>1</h1>
+                        <h1>300</h1>
+                    </div>
+                </div>
+                <div className="flex space-x-[15rem] flex-shrink justify-evenly">
+                    <div className="flex justify-center items-center">
+                    <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{
+                                duration: 0.8,
+                                delay: 0.5,
+                                ease: [0, 0.71, 0.2, 1.01]
+                            }}
+                        >
+                            <img src="images/image-9.png" alt="" />
+                        </motion.div>
+                        <h1 className="text-[36px]">Rice N Chicken</h1>
+                    </div>
+                    <div className="flex justify-center items-center space-x-[15rem]">
+                        <h1>1</h1>
+                        <h1>500</h1>
+                    </div>
+                </div>
+            </motion.div>
+            <motion.div 
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 1 }}
+            className="flex h-[51rem] px-[20px] space-y-[3rem] w-[40rem] text-white text-[40px] flex-col items-center bg-[#0F0808D4]">
+                <h1 className="font-semibold">Order Summary</h1>
+                <div className="flex space-x-[15rem]">
+                    <h1>3 Items</h1>
+                    <h1>1650</h1>
+                </div>
+                <div className="flex space-x-[15rem]">
+                    <h1>Shipping</h1>
+                    <h1>150</h1>
+                </div>
+                <div className="flex space-y-[0.5rem] flex-col justify-start items-start">
+                    <h1>Promo Code</h1>
+                    <input type="text" className="bg-[#D9D9D9] focus:text-black text-[36px] py-[0.5rem] px-[1rem] rounded-lg" placeholder="Enter Code" />
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                    >
+                        <input className="bg-[#EB002B] cursor-pointer py-[0.2rem] text-[36px] font-normal px-[1rem] rounded-full" type="button" value="Apply" />
+                    </motion.div>
+                </div>
+
+                <hr className="h-[1px] bg-[#FFFFFF] w-full" />
+
+                <div className="flex space-x-[15rem]">
+                    <h1>Total</h1>
+                    <h1>1850</h1>
+                </div>
+                <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                >
+                    <input className="cursor-pointer bg-[#EB002B] py-[0.5rem] mt-[2rem] text-[36px] font-normal px-[1rem] rounded-full" type="button" value="Proceed to Checkout" />
+                </motion.div>
+            </motion.div>
+        </div>
+    )
+}
