93043344d64e4b2b61f2690b9ab46e137f76c2e8
Muhammad Ansar <109860407+ansar062@users.noreply.github.com>
2023-11-03 15:28:49 +0500

    nothng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 import ProductItemCard from "./components/Cards/ProductItemCard";
+import Hero from "./components/hero";
 import MenuPage from "./components/menuPage";
 
 export default function Home() {
   return (
-    <div className=' min-h-screen '>
+    <div className='bg-[#FFD700] min-h-screen '>
       {/* <Navbar/> */}
       {/* <Hero/> */}
       {/* <Burger /> */}
-     {/* <Hero/> */}
+     <Hero />
    {/* <ShoppingCart /> */}
       {/* <Aboutus /> */}
      {/* <OrderCheckOut1 /> */}
      {/* <MenuPage /> */}
-     <ProductItemCard imgUrl='/images/beverages.png' name='Name' price='100' desc='DESCRIPTION'/>
+     {/* <ProductItemCard imgUrl='/images/beverages.png' name='Name' price='100' desc='DESCRIPTION'/> */}
      
     </div>
   )
